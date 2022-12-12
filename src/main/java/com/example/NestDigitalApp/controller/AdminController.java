c587195d47bd4bcd8a8ab9843629cd599bdd2bec
jobin <jobinjose.in@gmail.com>
2022-12-12 12:40:53 +0530

    Add-Employee, Employee-Login, Search-Employee

---
+++ src/main/java/com/example/NestDigitalApp/controller/AdminController.java
@@ -0,0 +1,53 @@
+package com.example.NestDigitalApp.controller;
+
+import com.example.NestDigitalApp.dao.EmployeeDao;
+import com.example.NestDigitalApp.dao.Leave1Dao;
+import com.example.NestDigitalApp.model.Employee;
+import com.example.NestDigitalApp.model.Leaves1;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+public class AdminController {
+    @Autowired
+    private EmployeeDao empdao;
+    @Autowired
+    private Leave1Dao l1dao;
+
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy");
+
+    @PostMapping(path = "/addEmployee", consumes = "application/json", produces = "application/json")
+    public HashMap<String, String> AddEmployee(@RequestBody Employee emp){
+        List<Employee> emp1 = (List<Employee>) empdao.UserLoginDetails(emp.getUsername(), emp.getPassword());
+        HashMap<String, String> hashMap = new HashMap<>();
+        if(emp1.size()==0){
+            LocalDateTime now = LocalDateTime.now();
+            empdao.save(emp);
+            Leaves1 l1 = new Leaves1();
+            l1.setEmpCode(emp.getEmpCode());
+            l1.setYear(dtf.format(now));
+            l1.setCasualLeave(20);
+            l1.setSickLeave(7);
+            l1.setSpecialLeave(3);
+            l1dao.save(l1);
+            hashMap.put("status","success");
+        }else{
+            hashMap.put("status","failed");
+        }
+        return hashMap;
+    }
+
+
+    @PostMapping(path = "/searchEmployee", consumes = "application/json", produces = "application/json")
+    public List<Employee> SearchEmployee(@RequestBody Employee emp){
+        return  (List<Employee>) empdao.SearchEmployee(emp.getName());
+    }
+
+}
--- src/main/java/com/example/NestDigitalApp/dao/EmployeeDao.java
+++ src/main/java/com/example/NestDigitalApp/dao/EmployeeDao.java
@@ -1,7 +1,16 @@
 package com.example.NestDigitalApp.dao;
 
 import com.example.NestDigitalApp.model.Employee;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeDao extends CrudRepository<Employee, Integer> {
+    @Query(value = "SELECT `id`, `designation`, `email`, `emp_code`, `name`, `password`, `phone`, `username` FROM `employee` WHERE `username`= :username AND `password`= :password", nativeQuery = true)
+    List<Employee> UserLoginDetails(@Param("username") String username, @Param("password") String password);
+
+    @Query(value = "SELECT `id`, `designation`, `email`, `emp_code`, `name`, `password`, `phone`, `username` FROM `employee` WHERE `name` LIKE %:name%", nativeQuery = true)
+    List<Employee> SearchEmployee(@Param("name") String name);
 }
