9c6edced137bad3197c5b80780b211d926dc71ca
jobin <jobinjose.in@gmail.com>
2022-12-14 09:59:54 +0530

    bugfixes

--- src/main/java/com/example/NestDigitalApp/controller/LeaveController.java
+++ src/main/java/com/example/NestDigitalApp/controller/LeaveController.java
@@ -31,11 +31,34 @@ public class LeaveController {
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/applyLeave", produces = "application/json", consumes = "application/json")
     public HashMap<String, String> ApplyLeave(@RequestBody LeaveModel lv){
-        LocalDateTime now = LocalDateTime.now();
-        lv.setApplyDate(dtf.format(now));
-        ldao.save(lv);
         HashMap<String, String> hashMap = new HashMap<>();
-        hashMap.put("status","success");
+        String leaveType = lv.getLeaveType();
+        List<Leaves1> l1 = (List<Leaves1>) l1dao.GetLeaveDetails(lv.getEmpId());
+        LocalDate dateBefore = LocalDate.parse(lv.getFromDate());
+        LocalDate dateAfter = LocalDate.parse(lv.getToDate());
+        int daysOfLeave = (int) ChronoUnit.DAYS.between(dateBefore, dateAfter)+1;
+        if(leaveType.equals("casualLeave") && (l1.get(0).getCasualLeave()-daysOfLeave)>=0){
+            LocalDateTime now = LocalDateTime.now();
+            lv.setApplyDate(dtf.format(now));
+            lv.setLeaveStatus(0);
+            ldao.save(lv);
+            hashMap.put("status","success");
+        } else if (leaveType.equals("sickLeave") && (l1.get(0).getSickLeave()-daysOfLeave)>=0) {
+            LocalDateTime now = LocalDateTime.now();
+            lv.setApplyDate(dtf.format(now));
+            lv.setLeaveStatus(0);
+            ldao.save(lv);
+            hashMap.put("status","success");
+        }else if (leaveType.equals("specialLeave") && (l1.get(0).getSpecialLeave()-daysOfLeave)>=0){
+            LocalDateTime now = LocalDateTime.now();
+            lv.setApplyDate(dtf.format(now));
+            lv.setLeaveStatus(0);
+            ldao.save(lv);
+            hashMap.put("status","success");
+        }
+        else{
+            hashMap.put("status","failed");
+        }
         return hashMap;
     }
 
@@ -54,40 +77,14 @@ public class LeaveController {
     @CrossOrigin(origins = "*")
     @PostMapping(path = "/updateLeaves", produces = "application/json", consumes = "application/json")
     public HashMap<String, String> UpdateLeaves(@RequestBody LeaveModel lv) throws ParseException {
-        String leaveType = lv.getLeaveType();
-        System.out.println(lv.getEmpId());
-        List<Leaves1> l1 = (List<Leaves1>) l1dao.GetLeaveDetails(lv.getEmpId());
-        System.out.println(l1.get(0).getSpecialLeave());
-        System.out.println(lv.getFromDate());
-        System.out.println(lv.getToDate());
-        String[] temp_from_date = lv.getFromDate().split("-", 3);
-        String from_date = temp_from_date[2]+"-"+temp_from_date[1]+"-"+temp_from_date[0];
-        String[] temp_to_date = lv.getToDate().split("-", 3);
-        String to_date = temp_to_date[2]+"-"+temp_to_date[1]+"-"+temp_to_date[0];
-        LocalDate dateBefore = LocalDate.parse(from_date);
-        LocalDate dateAfter = LocalDate.parse(to_date);
-        int daysOfLeave = (int) ChronoUnit.DAYS.between(dateBefore, dateAfter)+1;
         HashMap<String, String> hashMap = new HashMap<>();
-            if(leaveType.equals("casualLeave") && (l1.get(0).getCasualLeave()-daysOfLeave)>=0){
-                ldao.UpdateLeaves(lv.getId(), lv.getLeaveStatus());
-                l1dao.UpdateLeave(lv.getEmpId(),(l1.get(0).getCasualLeave()-daysOfLeave),l1.get(0).getSickLeave(),l1.get(0).getSpecialLeave());
-                hashMap.put("status","success");
-            } else if (leaveType.equals("sickLeave") && (l1.get(0).getSickLeave()-daysOfLeave)>=0) {
-                ldao.UpdateLeaves(lv.getId(), lv.getLeaveStatus());
-                l1dao.UpdateLeave(lv.getEmpId(),l1.get(0).getCasualLeave(),(l1.get(0).getSickLeave()-daysOfLeave),l1.get(0).getSpecialLeave());
-                hashMap.put("status","success");
-            }else if (leaveType.equals("specialLeave") && (l1.get(0).getSpecialLeave()-daysOfLeave)>=0){
-                ldao.UpdateLeaves(lv.getId(), lv.getLeaveStatus());
-                l1dao.UpdateLeave(lv.getEmpId(),l1.get(0).getCasualLeave(),l1.get(0).getSickLeave(),(l1.get(0).getSpecialLeave()-daysOfLeave));
-                hashMap.put("status","success");
-            }else if(lv.getLeaveStatus()==-1){
-                ldao.UpdateLeaves(lv.getId(), lv.getLeaveStatus());
-                hashMap.put("status","success");
-            }
-            else{
-                hashMap.put("status","failed");
-            }
-
+        List<Leaves1> l1 = (List<Leaves1>) l1dao.GetLeaveDetails(lv.getEmpId());
+        LocalDate dateBefore = LocalDate.parse(lv.getFromDate());
+        LocalDate dateAfter = LocalDate.parse(lv.getToDate());
+        int daysOfLeave = (int) ChronoUnit.DAYS.between(dateBefore, dateAfter)+1;
+        ldao.UpdateLeaves(lv.getId(), lv.getLeaveStatus());
+        l1dao.UpdateLeave(lv.getEmpId(),(l1.get(0).getCasualLeave()-daysOfLeave),l1.get(0).getSickLeave(),l1.get(0).getSpecialLeave());
+        hashMap.put("status","success");
         return hashMap;
     }
 
